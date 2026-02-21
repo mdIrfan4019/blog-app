744ad89dcb7d76d3cd1b8127d88d979da51fd1e9
Md Irfan <mia2823457@gmail.com>
2026-02-21 11:27:47 +0530

    issue of cors fixed

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://blog-app-xi-seven-68.vercel.app/"
+    "https://blog-app-xi-seven-68.vercel.app"
   ],
   credentials: true
 }));
